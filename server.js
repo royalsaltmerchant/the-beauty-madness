ef34e5dff61285bd7e48ae351afd9ea4c6d90052
Julian <jumpingafterrain@gmail.com>
2023-01-04 16:15:43 +0100

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const routes = require("./src/api/routes.js")
+
+var app = express()
+
+//Set CORS
+app.use(cors())
+
+//Set JSON parser
+// app.use(express.json())
+
+// Body Parse
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api', routes)
+// Static 
+app.use(express.static('public'))
+
+//Error
+app.use((error, req, res, next) => {
+  console.error(error)
+  // code for unique constraint on user registration email
+  if(error.code == 23505) error.status = 400
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message || "Internal Server Error"
+    }
+  })
+})
+
+//Run
+var PORT = 80
+app.listen({port: PORT}, async () => {
+  console.log(`Server Running at http://localhost:${PORT}`)
+})
